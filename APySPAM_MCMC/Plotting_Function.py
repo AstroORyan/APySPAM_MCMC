I believe I have fixed orientation issue. This came down to overcomplication and an incorrect resolution calculation.

APySPAM_MCMC/Plotting_Function.py
@@ -8,6 +8,7 @@ Quickly creates an image of the system using imshow and a meshgrid.
 
 """
 import numpy as np
+import sys
 
 class Plotting_Function:
     def plotting(Coordinates,part_flux,SFRs,n_filters,Resolution,dims,Conversion):
@@ -47,7 +48,7 @@ class Plotting_Function:
                             
         #plt.figure()
         #plt.imshow(Image.T, origin='lower')
-        #plt.title('White Image')p,q
+        #plt.title('White Image')
         
         #plt.figure()
         #plt.imshow(-2.5*np.log10(Image.T) - 48.6, origin='lower')

APySPAM_MCMC/Run_MCMC.py
@@ -156,7 +156,7 @@ def lnprob(theta,Input_Image,Input_Binary,Sigma_Image):
     # As long as priors are satisfied, run the APySPAM simulation.
     #print('Simulation started at: ', datetime.datetime.now())
     candidate_sim_image = Run.main(theta,Conversion,Resolution,filters,[Input_Image.shape[0],Input_Image.shape[1]],Spectral_Density_1,Spectral_Density_2)
-    candidate_sim_image = np.flip(np.rot90(candidate_sim_image,1),axis=1)
+    #candidate_sim_image = np.flip(candidate_sim_image,axis=1)
     #print('Simulation finished at: ', datetime.datetime.now())
     # candidate_sim_image = ndimage.gaussian_filter(candidate_sim_image,sigma=0.5,mode='nearest')
     
@@ -193,12 +193,12 @@ def lnprob(theta,Input_Image,Input_Binary,Sigma_Image):
 #    plt.imshow(-2.5*np.log10(Input_Image) - 48.6)
 #    plt.title('Input')
 #    plt.savefig(r'/mmfs1/home/users/oryan/PySPAM_Original_Python_MCMC_Full/Results/Input.png')
-#    if ln_like >= -10:
-    output_name = str(uuid.uuid4())
-    plt.figure()
-    plt.imshow(-2.5*np.log10(candidate_sim_image) - 48.6)
-    plt.title(['Sim. Ln_like = ', str(ln_like)])
-    plt.savefig(f'/mmfs1/home/users/oryan/PySPAM_Original_Python_MCMC_Full/Results/{output_name}.png')
+    if ln_like >= -5:
+        output_name = str(uuid.uuid4())
+        plt.figure()
+        plt.imshow(-2.5*np.log10(candidate_sim_image) - 48.6)
+        plt.title(['Sim. Ln_like = ', str(ln_like)])
+        plt.savefig(f'/mmfs1/home/users/oryan/PySPAM_Original_Python_MCMC_Full/Results/{output_name}.png')
     
 #    plt.figure()
 #    plt.imshow(Input_Binary)

APySPAM_MCMC/Secondary_Placer.py
@@ -11,6 +11,7 @@ narrow prior for the secondary galaxy x and y positions.
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 def Distance_Calc(z):
       Omega_Matter = 0.308   #These values utilise a Reference Frame defined by the NASA ED
@@ -82,7 +83,7 @@ def Secondary_Placer(Input_Image,Input_Image_Binary,z,block_reduce,Name):
     # block_reduce = 6
     d = Distance_Calc(z)
     pixel_resolution = (0.396/(3600))*(np.pi/180)*block_reduce
-    Resolution = d*np.tan(pixel_resolution)
+    Resolution = 2*d*np.tan(pixel_resolution)
     
     # Then, define the 10x10 grid of ones
     if Input_Image.shape[0] == 50:
@@ -184,11 +185,11 @@ def Secondary_Placer(Input_Image,Input_Image_Binary,z,block_reduce,Name):
     plt.close()
     
     # Convert bin positions into Primaries Frame
-    Conversion = [Input_Image.shape[1]/2 - Position_Prim[1], Input_Image.shape[0]/2 - Position_Prim[0]]
+    Conversion = [Position_Prim[1] - Input_Image.shape[1]/2, Position_Prim[0] - Input_Image.shape[0]/2]
     
     # Convert into Galaxy Unit Distances and into the frame of the Primary 
-    Position[0] = ((Position_Sec[0] - Conversion[0]))*(Resolution/DU)
-    Position[1] = ((Position_Sec[1] - Conversion[1]))*(Resolution/DU)
+    Position[0] = ((Position_Sec[0] + Conversion[0]))*(Resolution/DU)
+    Position[1] = ((Position_Sec[1] + Conversion[1]))*(Resolution/DU)
     
     # Check Orientation:
 #    if Position_Sec[0] > Position_Prim[0]:
@@ -199,6 +200,6 @@ def Secondary_Placer(Input_Image,Input_Image_Binary,z,block_reduce,Name):
     # Prepare Stuff to export:
     x = Position[0]
     y = Position[1]
-        
+
     # Return
     return Conversion,[x,y], [x-0.5,x+0.5,y-0.5,y+0.5],Resolution