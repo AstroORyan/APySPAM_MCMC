8eae3fdd2e9d2482d7c62f9ee9a062402a894864
O'ryan <oryan@lancaster.ac.uk>
2022-10-05 10:13:02 +0100

    Have begun implementation of SBI script. Added testing Notebook.

--- APySPAM_MCMC/Run_MCMC.py
+++ APySPAM_MCMC/Run_MCMC.py
@@ -4,145 +4,31 @@ Created on Mon Sep 13 17:21:55 2021
 
 @author: oryan
 
-This is going to be the integration script with APySPAM in its new form. The new form is much more efficinet and fast.
+The Simulation-Based Inference version of APySPAM MCMC. This algorithm uses a combination of MCMC and neural networks in order to explore the posterior distribution of the parameters.
 
 """
-# Required Packages
-import glob
+# Imports
 import numpy as np
-import emcee
+
+import os
+import torch
+
+from sbi import utils as utils
+from sbi import analysis as analysis
+from sbi.inference import SNPE, prepare_for_sbi, simulate_for_sbi
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import datetime
-from scipy import ndimage
-from pandas import read_csv
-import uuid
-import os
-import time
-from multiprocessing import Pool
-import sys
-import corner
-from astropy.stats import sigma_clip
 
-# Required Scripts Import
+# Required Scripts
 from Run import Run
 from Import_Procedure import Imports
 from Setup_Parameters import Setup_Parameters
 from Secondary_Placer import Secondary_Placer
 from colour import colour
 
-class Main_Script:
-    def MCMC(ndim, nwalkers, nsteps, start, Input_Image, Sigma_Image, Gal_Name):
-        
-        cwd = os.getcwd() + '/PySPAM_Original_Python_MCMC'
-        
-        p0 = [(start + np.concatenate([2.5*(-1+2*np.random.random(1)),1.5*(-1+2*np.random.random(3)),2.4*(-1+2*np.random.random(2)),
-                                      1.5*(-1+2*np.random.random(2)),45*(-1+2*np.random.random(2)),45*(-1+2*np.random.random(2)), (-1+2*np.random.random(1))])) for i in range(nwalkers)]
-        
-        print('Beginning Burnin of Input ',Gal_Name,'.')
-
-        filename = f'{cwd}/Results/' + Gal_Name + '_Full.h5'
-        backend = emcee.backends.HDFBackend(filename)
-        backend.reset(nwalkers,ndim)
-
-        # Initiate the burn in phase utilising a Differential Evolution Move in the sampler.
-        move = [(emcee.moves.DEMove(), 0.8), (emcee.moves.DESnookerMove(), 0.2),]
-        print('Made it to setting up the Pool!')
-        with Pool(processes=16) as pool:
-            sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=([Input_Image,Sigma_Image]),moves=move,pool=pool,backend=backend)
-            sampler.run_mcmc(p0,nsteps,progress=True)
-            pool.close()
-            pool.join()
-
-        time.sleep(10)
-
-        tau = sampler.get_autocorr_time(quiet=True)
-        burnin = int(2 * np.nanmax(tau))
-        thin = int(0.5 * np.nanmin(tau))
-
-        samples = sampler.get_chain(discard=burnin, flat=True, thin=thin)
-        log_prob_samples = sampler.get_log_prob(discard=burnin,flat=True,thin=thin)
-        #log_prior_samples = sampler.get_blobs(discard=burnin,flat=True,thin=thin)
-
-        print("Burnin-in: {0}".format(burnin))
-        print("thin: {0}".format(thin))
-        print("Flat chain shape: {0}".format(samples.shape))
-        print("flat log prob shape: {0}".format(log_prob_samples.shape))
-        #print("flat log prior shape: {0}".format(log_prior_samples.shape))
-
-        all_samples = np.concatenate((samples, log_prob_samples[:,None]), axis = 1)
-
-        try:
-            filename = '{cwd}/Results/Run_Samples_'+Gal_Name+'.npy'
-            np.save(filename,all_samples)
-        except:
-            output_name = str(uuid.uuid4())
-            filename = f'{cwd}/Results/Run_Samples_{output_name}.npy'
-            print(f'WARNING! Savename didn\'t work. Have saved current run as {output_name}')
-            np.save(filename,all_samples)
-        
-        return all_samples[:,:ndim]
-        
-    def Prior(theta,dims):
-        #x,y = theta[0:2]
-        z = theta[0]
-        v = theta[1:4]
-        m1, m2, r1, r2, p1, p2, t1, t2, t = theta[4:]
-        phis = np.asarray([p1,p2])
-        thetas = np.asarray([t1,t2])
-        if any(np.asarray([m1,m2,r1,r2]) < 0.0):    # Negative values for these parameters obviously break the code and are unphysical.
-            Chi_Squared = -np.inf
-        # elif (x < Limits[0]) or (x > Limits[1]) or (y < Limits[2]) or (y > Limits[3]):
-        #     Chi_Squared = -np.inf
-        elif z < -10 or z > 10:
-            Chi_Squared = -np.inf
-        elif v[0] < -10 or v[0] > 10:
-            Chi_Squared = -np.inf
-        elif v[1] < -10 or v[1] > 10:
-            Chi_Squared = -np.inf
-        elif v[2] > 10 or v[2] < -10:
-            Chi_Squared = -np.inf
-        elif np.linalg.norm(v) == 0.0:
-            Chi_Squared = -7e6
-        # elif np.linalg.norm([x,y,z]) == 0.0:
-        #     Chi_Squared = -7e6
-        elif m1 > 10 or m2 > 10 or m1 <= 0.01 or m2 <= 0.01:      # Note, this is an upper limit on mass of 5x10^12 Solar Masses.
-            Chi_Squared = -np.inf
-        elif r1 > 4 or r2 > 4 or r1 <= 0.01 or r2 <= 0.01:    
-            Chi_Squared = -np.inf            
-        elif any(phis > 180) or any(phis < 0.0) or any(thetas < 0.0) or any(thetas > 180):
-            Chi_Squared = -np.inf
-        elif int(1000*(m1/(m1 + m2))) == 0 or int(1000*(m2/(m1 + m2))) == 0:
-            Chi_Squared = -np.inf         
-        elif m1/r1 > 5 or m2/r2 > 5: 	# This says, if I have a maximum mass of 5e11 SM, then my minimum radius is 15kpc.
-            Chi_Squared = -np.inf
-        elif m1/r1 < 0.5 or m2/r2 < 0.5:  # This says, if I have a minimum mass of 1e10, the maximum radius it can have is 5kpc.
-            Chi_Squared = -np.inf
-        elif t > -3 or t < -7:
-            Chi_Squared = -np.inf
-        else: 
-            Chi_Squared = 0
-
-        m1 = abs(m1)
-        m2 = abs(m2)
-        r1 = abs(r1)
-        r2 = abs(r2)
-        p1 = abs(p1)
-        p2 = abs(p2)
-        t1 = abs(t1)
-        t2 = abs(t2)
-        if t > 0:
-             t = -t
-        if t < -3:
-             t = -5
-        if t > -7:
-             t = -5
-        theta = np.array([z,v[0],v[1],v[2],m1,m2,r1,r2,p1,p2,t1,t2,t])
-                
-        return Chi_Squared, theta
-
-    
+## Main Functions
 def Sigma_Calc(Input_Image):
     # Artificial Cleaning of Input 
     input_image = Input_Image.copy()
@@ -155,153 +41,83 @@ def Sigma_Calc(Input_Image):
     
     return sigma_image_exp
 
-    
-def lnprob(theta,Input_Image,Sigma_Image):
-    ln_prior, theta = Main_Script.Prior(theta,[Input_Image.shape[0],Input_Image.shape[1]])
-    
-    if ln_prior == -np.inf:
-        return -np.inf
-
-    # As long as priors are satisfied, run the APySPAM simulation.
-    #print('Simulation started at: ', datetime.datetime.now())
-    sim_image, sim_image_flip_0, sim_image_flip_1, no_int_flag = Run.main(theta,xy_pos,Resolution,filters,[Input_Image.shape[0],Input_Image.shape[1]],Spectral_Density_1,Spectral_Density_2,z)
-    
-    if no_int_flag:
-        return -np.inf
-    
-    # Now have candidate simulation image. Want to compare to observation using a Chi Squared.
-    N = Input_Image.shape[0]*Input_Image.shape[1]
-    Sigma_Array = (Input_Image - sim_image)**2/(2*(Sigma_Image**2))
-    Chi_Squared = (1/N)*np.sum(Sigma_Array)
-    
-    Sigma_Array_flip_0 = (Input_Image - sim_image_flip_0)**2 / (2*Sigma_Image**2)
-    Chi_Sqr_flip_0 = (1/N)*np.sum(Sigma_Array_flip_0)
-    
-    Sigma_Array_flip_1 = (Input_Image - sim_image_flip_1)**2 / (2 * Sigma_Image**2)
-    Chi_Sqr_flip_1 = (1/N)*np.sum(Sigma_Array_flip_1)
-    
-    if Chi_Squared > Chi_Sqr_flip_0:
-        Chi_Squared = Chi_Sqr_flip_0
-    
-    if Chi_Squared > Chi_Sqr_flip_1:
-        Chi_Squared = Chi_Sqr_flip_1
-    
-    mask = sim_image > 0
-
-    # Now, use our likelihood function to see the probability that these parameters (and simualted image) represent the observed data.
-    ln_like = -(Chi_Squared/2) + ln_prior + 1
-    
-
-## Testing Lines
-#    if ln_like >= -2.0:
-#        output_name = str(uuid.uuid4())
-#        plt.figure()
-#        plt.imshow(-2.5*np.log10(candidate_sim_image) - 48.6)
-#        plt.title(['Sim. Ln_like = ', str(ln_like)])
-#        plt.savefig(f'/mmfs1/home/users/oryan/PySPAM_Original_Python_MCMC_Full/Test_Images/{output_name}.png')
-#        plt.close()
-
-    if np.isnan(ln_like):
-        ln_like = -np.inf
-    
-    return ln_like
-
 def Observation_Import(path):
     Input_Image = np.load(path)
     
     Galaxy_Name = os.path.splitext(os.path.basename(path))[0]
         
     return Input_Image, Galaxy_Name
+
+def get_priors():
+    ### [z, vx, vy, vz, M1, M2, R1, R2, phi1, phi2, theta1, theta2, time]
+    min_priors = [-20, -20, -20, -20, 0.01, 0.01, 0.01, 0.01, 0, 0, 0, 0, -7]
+    max_priors = [20, 20, 20, 20, 10, 10, 4, 4, 360, 360, 360, 360, -3]
+
+    prior = utils.torchutils.BoxUniform(
+        low = torch.as_tensor(min_priors),
+        high = torch.as_tensor(max_priors)
+    )
+
+    return prior
+
+def simulator(params):
+
+    params_arr = np.array(params)
+
+    sim_im, no_int_flag = Run(params_arr, xy_pos, res, filters, input_im.shape, spec_data_1, spec_data_2, z)
+
+    if no_int_flag:
+        return np.zeros(sim_im.shape) * 1e70
     
-def star_remove(im):
-    clipped_image = sigma_clip(im, sigma=10,maxiters=1,cenfunc='median')
-    mask = clipped_image.mask
-
-    stellar_pixels = np.where(mask)
-
-    for i in range(len(stellar_pixels[0])):
-        x = stellar_pixels[0][i]
-        y = stellar_pixels[1][i]
-
-        if x - 3 < 0:
-            x = x + 3
-        if x + 3 > im.shape[0]:
-            x = im.shape[0] - 3
-        if y + 3 < 0:
-            y = y + 3
-        if y + 3 > im.shape[1]:
-            y = im.shape[1] - 3
-
-        replacement = np.median(im[x-3:x+3,y-3:y+3])*np.random.random([6,6])
-
-        im[x-3:x+3,y-3:y+3] = replacement
-
-    return im
-
-def Binary_Creator(Image):
-    Input_Image_Mags = -2.5*np.log10(Image) - 48.6
+    return sim_im
     
-    Input_Image_Mags[np.isnan(Input_Image_Mags)] = 30
-    
-    cutoff_max = 16
-    cutoff_min = 21
-    
-    Input_Image_Binary = Input_Image_Mags.copy()
-    Input_Image_Binary[Input_Image_Binary < cutoff_max] = 0
-    Input_Image_Binary[Input_Image_Binary > cutoff_min] = 0
-    Input_Image_Binary[Input_Image_Binary > 0] = 1
+    # sim_im_flip_0 = np.flip(sim_im, 0)
+    # sim_im_flip_1 = np.flip(sim_im, 1)
 
-    return Input_Image_Binary
+    # N = sim_im.shape[0] * sim_im.shape[1]
 
+    # c_sqr_0 = (1/N) * (((input_im - sim_im)**2) / ((2 * sigma_im)**2))
+    # c_sqr_1 = (1/N) * (((input_im - sim_im_flip_0)**2) / ((2 * sigma_im)**2))
+    # c_sqr_2 = (1/N) * (((input_im - sim_im_flip_1)**2) / ((2 * sigma_im)**2))
+
+    # c_sqr = np.min([c_sqr_0, c_sqr_1, c_sqr_2])
+
+    # ln_like = -(c_sqr/2)
+
+    return ln_like
+
+## Main Function
 def Run_MCMC():
-    global Resolution, filters, block_reduce, Spectral_Density_1, Spectral_Density_2,z,xy_pos
-    # Setup inputs and imports that can be done before iterating through MCMC
-    
-    cwd = os.getcwd() + '/PySPAM_Original_Python_MCMC' # To run locally, remove this addition.
-    
-    input_folder = f'{cwd}/All_Inputs/'
-    input_paths = glob.glob(input_folder+'*.*')
-    n_inputs = len(input_paths)
-    filters = colour.get_filters(cwd)
-    
-    if len(filters) < 0.5:
-        print('WARNING: No Filters input.')
-        sys.exit()
-    
-    redshifts = read_csv(f'{cwd}/Redshifts/Redshifts.csv')
+    global xy_pos, input_im, sigma_im, res, filters, spec_data_1, spec_data_2, z
 
-    
-    # Setup MCMC run
-    ndim = 13
-    nwalkers = 80
-    nsteps = 5000
+    ## Old Stuff, get Simulation Parameters above like in the original PySPAM. Just need to call the right functions
 
-    Spectral_Density_1 = np.loadtxt(f'{cwd}/Spectra/Raw_Spectral_Data_Z_0.0001.txt')
-    Spectral_Density_2 = np.loadtxt(f'{cwd}/Spectra/Raw_Spectral_Data_Z_0.004.txt')
-    
-    # The MCMC Run
-    for p in range(0,5):
-        print('Beginning run for ', input_paths[p])
-        Input_Image, Name = Observation_Import(input_paths[p])
-        #Input_Image = star_remove(Input_Image)
-        Sigma_Image = Sigma_Calc(Input_Image)
-        Input_Binary = Binary_Creator(Input_Image)
-        xy_pos, Resolution, z, skip_flag = Secondary_Placer.get_secondary_coords(Name, redshifts)
-        
-        if skip_flag:
-            continue
+    ## New Stuff, Setting up Simulations
+    priors = get_priors()
 
-        start = Setup_Parameters.Starting_Locations()
-        samples = Main_Script.MCMC(ndim, nwalkers, nsteps, start, Input_Image, Sigma_Image,Name)
-        filename = '/mmfs1/home/users/oryan/PySPAM_Original_Python_MCMC_Full/Results/Main_Corner_Plot_'+Name+'.png'
-        Labels = ['z','vx','vy','vz','M1','M2','R1','R2','phi1','phi2','theta1','theta2','t']
-        fig = corner.corner(samples,labels=Labels,show_titles=True)
-        fig.savefig(filename)
-        plt.close()
+    simulator, prior = prepare_for_sbi(simulator, prior)
 
-        del Input_Image, start, Input_Binary, Sigma_Image, samples
+    inference = SNPE(prior = prior)
 
-    
+    theta, x = simulate_for_sbi(simulator, proposal = prior, num_simulations = 1000)
 
+    inference = inference.append_simulations(theta, x)
+
+    density_estimator = inference.train()
+
+    posterior = inference.build_posterior(density_estimator)
+
+    posterior_samples = posterior.sample((10000,), x = input_im)
+
+    _ = analysis.pairplot(
+        posterior_samples, 
+        limits = [[-20,20], [-20,20], [-20,20], [-20,20], [0.01,10], [0.01,10],[0.01,4],[0.01,4],[0,360],[0,360],[0,360],[0,360],[-7,-3]],
+        figsize = (10,10)
+    )
+
+    plt.gca()
+    plt.savefig('/mmfs1/home/users/oryan/APySPAM_SBI/test.jpeg')
+
+## Initialization
 if __name__ == "__main__" : 
     Run_MCMC()
